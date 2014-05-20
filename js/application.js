0918d7d4a45a0e20e96ce4c3c4f3b80627723d58
ingikim <bcmet365@daum.net>
2014-05-20 16:07:43 +0900

    alpha3

--- js/application.js
+++ js/application.js
@@ -3,14 +3,3 @@ window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
 
-$(document).ready(function(){
-	$('.slider').slick({
-	  dots: true,
-  	infinite: true,
-  	speed: 500,
-  	fade: true,
-  	slide: '> div',
-  	cssEase: 'linear'
-	});
-});
-
